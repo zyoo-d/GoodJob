8bb9f03de2e899e93bb104133140ec4a479a6624
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-04-25 17:49:39 +0900

    DB 데이터 추가중

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/5e/50ba13ebcb02001f1f27ca3a222e284a
@@ -0,0 +1,13 @@
+package com.goodjob;
+
+import lombok.Data;
+
+@Data
+public class Company {
+	
+	private String seq;
+	private String code;
+	private String name;
+	private String est_dt;
+	private String ceo_nm;
+}
---
+++ GoodJob/src/main/java/com/goodjob/Finance.java
@@ -0,0 +1,24 @@
+package com.goodjob;
+
+import lombok.Data;
+
+@Data
+public class Finance {
+	private String fnc_income;
+	private String fnc_sales;
+	private String fnc_ebit;
+	private String fnc_period;
+	
+	private String p_income;
+	private String p_sales;
+	private String p_ebit;
+	private String p_period;
+	
+	private String ol_income;
+	private String ol_sales;
+	private String ol_ebit;
+	private String ol_period;
+	
+	private String seq;
+	
+}
