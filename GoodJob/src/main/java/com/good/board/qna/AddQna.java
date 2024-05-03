b17c9c9c6d8b75fabf08feeed41580140cc7de4c
zyoo-d <ljiyxx96@gmail.com>
2024-05-03 10:01:34 +0900

    qna

--- GoodJob/src/main/java/com/good/board/qna/AddQna.java
+++ GoodJob/src/main/java/com/good/board/qna/AddQna.java
@@ -1,5 +1,22 @@
-package com.good.board.qna;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-public class AddQna {
+@WebServlet("/user/qna/addqna.do")
+public class AddQna extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/qna/addqna.jsp");
+		dispatcher.forward(req, resp);
+		
+	}
 
 }
