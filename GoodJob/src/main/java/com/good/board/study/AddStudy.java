0145a125b46fd6d1afd91b1ce7de3f0d227c172c
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-05-04 12:01:32 +0900

    기업 검색 자동완성 구현

--- GoodJob/src/main/java/com/good/board/study/AddStudy.java
+++ GoodJob/src/main/java/com/good/board/study/AddStudy.java
@@ -1,5 +1,32 @@
 package com.good.board.study;
 
-public class AddStudy {
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/addstudy.do")
+public class AddStudy extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/study/addstudy.jsp");
+		dispatcher.forward(req, resp);
+
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		
+		
+		
+		
+	}
 
 }
