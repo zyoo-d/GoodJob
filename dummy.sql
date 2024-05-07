dc0004ef96d971602c4f45bde47783302cb29076
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-05-08 00:29:28 +0900

    Create dummy.sql

---
+++ dummy.sql
@@ -0,0 +1,24 @@
+select c.cp_name, cr.* from tblcompanyreview cr
+    inner join tblcompany c 
+        on cr.cp_seq = c.cp_seq;
+        commit;
+update tblcompanyreview set growth_score = 5 where growth_score = 6;
+insert into tblcompanyreview values 
+(seqcompanyreview.nextval, 5, 4, 3, 4, 4, '전반적으로 괜찮아요.', '연봉이 맛있음', 
+'야근이 맛없음', '2024-04-10', 2105, 'wantjob', 1);
+select * from tblhire h
+    inner join tblcompany c
+        on h.cp_seq = c.cp_seq
+            where h.hire_avr_year > 0;
+
+select * from tbluser;
+update tbluser set pw = 123 where id = 'wantjob';
+commit;
+
+select months_between(sysdate, founded) as diff from tblcompany
+    where founded is not null;
+
+select * from tblhire h
+    inner join tblcompany c
+        on h.cp_seq = c.cp_seq
+            where h.hire_retired > h.hire_new and h.hire_member > 50;
