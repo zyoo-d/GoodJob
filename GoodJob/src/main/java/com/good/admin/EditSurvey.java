8b43d9e0ead52a516be4980763eba1f905c7f60c
Heey2 <155238712+Heey2@users.noreply.github.com>
2024-05-15 15:27:08 +0900

    heeyeon 개인 파일 정리 + doc 주석

--- GoodJob/src/main/java/com/good/admin/EditSurvey.java
+++ GoodJob/src/main/java/com/good/admin/EditSurvey.java
@@ -13,32 +13,44 @@ import com.good.alert.Alert;
 import com.good.matching.model.SurveyDTO;
 import com.good.matching.repository.SurveyDAO;
 
+/**
+ * EditSurvey 서블릿 클래스는 설문 조사 항목을 편집하는 요청을 처리합니다. 이 서블릿은
+ * "/admin/survey/editsurvey.do" URL 패턴으로 매핑되어 있습니다.
+ */
 @WebServlet("/admin/survey/editsurvey.do")
 public class EditSurvey extends HttpServlet {
 
+	/**
+	 * GET 요청을 통해 설문 조사의 특정 항목을 수정합니다. 사용자는 설문 조사의 항목 번호(sv_seq)와 수정할
+	 * 내용(sv_content)을 제공해야 합니다.
+	 *
+	 * @param req  클라이언트로부터 받은 HttpServletRequest 객체, 요청 데이터 접근에 사용됩니다.
+	 * @param resp 클라이언트에게 보낼 HttpServletResponse 객체, 응답 데이터 설정에 사용됩니다.
+	 * @throws ServletException 서블릿 처리 중 예외가 발생할 경우 던져집니다.
+	 * @throws IOException      입출력 예외 발생시 던져집니다.
+	 */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		String sv_seq = req.getParameter("sv_seq");
 		String sv_content = req.getParameter("sv_content");
-		
+
 		SurveyDTO dto = new SurveyDTO();
 		dto.setSv_seq(sv_seq);
 		dto.setSv_content(sv_content);
-		
+
 		SurveyDAO dao = new SurveyDAO();
 		int result = dao.editQuestion(dto);
 		dao.close();
-		
-		if(result == 1) {
+
+		if (result == 1) {
 			RequestDispatcher dispatcher = req.getRequestDispatcher("/admin/survey/survey.do");
 			dispatcher.forward(req, resp);
 		} else {
 			Alert.fail(resp);
 		}
-		
-		dao.close();
 
+		dao.close();
 
 	}
 
--- GoodJob/src/main/java/com/good/board/study/AuthStudy.java
+++ GoodJob/src/main/java/com/good/board/study/AuthStudy.java
@@ -1,7 +1,6 @@
 package com.good.board.study;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,9 +9,19 @@ import javax.servlet.http.HttpSession;
 import com.good.alert.Alert;
 import com.good.board.model.StudyDTO;
 import com.good.board.repository.StudyDAO;
-
+/**
+ * AuthStudy 클래스는 사용자의 인증을 검사하여 특정 공부 세션에 대한 접근 권한을 확인합니다.
+ */
 public class AuthStudy {
-
+	/**
+     * 사용자가 특정 공부 세션에 대한 수정 권한을 가지고 있는지 검사합니다.
+     * 사용자가 세션 소유자이거나 관리자 레벨을 가진 경우에만 허용합니다.
+     * 
+     * @param req HttpServletRequest 객체, 사용자 요청과 관련된 데이터 접근에 사용됩니다.
+     * @param resp HttpServletResponse 객체, 필요한 경우 사용자에게 응답을 보냅니다.
+     * @return boolean 권한이 없는 경우 true를 반환하고, 권한이 있는 경우 false를 반환합니다.
+     * @throws IOException 네트워크 또는 서버 문제로 인해 입출력 문제가 발생할 경우 예외를 던집니다.
+     */
 	public static boolean check(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
 		HttpSession session = req.getSession();
