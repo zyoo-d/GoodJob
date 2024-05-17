175b310ca245c86f446839a03e10ee5780b5031f
jub <js4real@naver.com>
2024-05-17 11:14:24 +0900

    개발문서

--- GoodJob/src/main/java/com/good/user/ChangePw.java
+++ GoodJob/src/main/java/com/good/user/ChangePw.java
@@ -13,10 +13,20 @@ import org.json.simple.JSONObject;
 
 import com.good.user.model.UserDTO;
 import com.good.user.repository.UserDAO;
-
+/**
+ * ChangePw 서블릿은 사용자의 비밀번호를 변경하는 기능을 제공합니다.
+ * 클라이언트로부터 사용자의 ID와 변경할 비밀번호를 받아와 데이터베이스에 저장된 해당 사용자의 비밀번호를 변경합니다.
+ */
 @WebServlet("/user/changepw.do")
 public class ChangePw extends HttpServlet {
-	
+	  /**
+     * POST 요청을 처리하여 사용자의 비밀번호를 변경합니다.
+     * 
+     * @param req HttpServletRequest 객체로 클라이언트로부터 전송된 요청을 나타냅니다.
+     * @param resp HttpServletResponse 객체로 클라이언트로 응답을 보냅니다.
+     * @throws ServletException 서블릿 관련 예외가 발생할 경우 던져집니다.
+     * @throws IOException 입출력 관련 예외가 발생할 경우 던져집니다.
+     */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String id = req.getParameter("id");
--- GoodJob/src/main/java/com/good/user/GetId.java
+++ GoodJob/src/main/java/com/good/user/GetId.java
@@ -13,10 +13,19 @@ import org.json.simple.JSONObject;
 
 import com.good.user.model.UserDTO;
 import com.good.user.repository.UserDAO;
-
+/**
+ * GetId 서블릿은 사용자의 이름과 전화번호를 받아와 해당 사용자의 아이디를 반환합니다.
+ */
 @WebServlet("/user/getid.do")
 public class GetId extends HttpServlet {
-	
+    /**
+     * POST 요청을 처리하여 사용자의 이름과 전화번호를 받아와 해당 사용자의 아이디를 반환합니다.
+     * 
+     * @param req HttpServletRequest 객체로 클라이언트로부터 전송된 요청을 나타냅니다.
+     * @param resp HttpServletResponse 객체로 클라이언트로 응답을 보냅니다.
+     * @throws ServletException 서블릿 관련 예외가 발생할 경우 던져집니다.
+     * @throws IOException 입출력 관련 예외가 발생할 경우 던져집니다.
+     */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
